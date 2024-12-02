b05f475a19169d734f0d12a277d1b9fd088eba64
furrzik <vadimhohlacev@gmail.com>
2024-12-02 20:30:48 +0300

    Задания

---
+++ Проект/arithmetic.py
@@ -0,0 +1,13 @@
+pravilniy_otvet = 4 * 100 - 54
+
+otvet_polzovotelya = input("Решите пример 4*100-54:")
+
+otvet_polzovotelya = int(otvet_polzovotelya)
+print(f"\nВаш ответ: {otvet_polzovotelya}")
+print(f"Правильный ответ: {pravilniy_otvet}")
+if otvet_polzovotelya == pravilniy_otvet:
+    print('Верно')
+else:
+    print("Неверно")
+        
+
